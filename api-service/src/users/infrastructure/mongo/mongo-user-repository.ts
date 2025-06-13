6861f77a62e1c7c0be7cbe6b1ed84350fee8ade5
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-13 21:36:39 +0200

    feat: Implement MongoUserRepository for user data persistence

---
+++ api-service/src/users/infrastructure/mongo/mongo-user-repository.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserRepository } from '../../domain/user-repository';
+import { User } from '../../domain/user';
+import { UserDocument } from './user-schema';
+
+@Injectable()
+export class MongoUserRepository implements UserRepository {
+    constructor(
+        @InjectModel('User') private readonly userModel: Model<UserDocument>,
+    ) {}
+
+    async save(name: string): Promise<void> {
+        await this.userModel.insertOne(
+            {
+                name,
+            },
+        );
+    }
+
+    async findByName(name: string): Promise<User> {
+        const user = await this.userModel.findOne({
+            name,
+        });
+
+        if (!user) {
+            throw new Error(`User with name ${name} not found`);
+        }
+
+        return new User(user.id, user.name);
+    }
+}

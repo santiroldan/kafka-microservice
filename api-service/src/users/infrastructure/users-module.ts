7feea21d5d4b02d4e3b591cf0ca7fd4db74d33d2
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-13 21:36:55 +0200

    feat: Create UsersModule for user management and integration with Mongoose

---
+++ api-service/src/users/infrastructure/users-module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserCreator } from "../application/user-creator";
+import { USER_REPOSITORY } from "../domain/user-repository";
+import { PostUserController } from "./http/post-user-controller";
+import { MongoUserRepository } from "./mongo/mongo-user-repository";
+import { UserSchema } from "./mongo/user-schema";
+
+export const USERS_PROVIDERS = [
+	{
+		provide: USER_REPOSITORY,
+		useClass: MongoUserRepository,
+	},
+	UserCreator,
+];
+
+@Module({
+	controllers: [PostUserController],
+	imports: [MongooseModule.forFeature([{ name: "User", schema: UserSchema }])],
+	providers: [...USERS_PROVIDERS],
+	exports: [...USERS_PROVIDERS],
+})
+export class UsersModule {}

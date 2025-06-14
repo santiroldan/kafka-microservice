cd95d751a4f084f34c36f349f553846e747eeefc
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-15 01:49:59 +0200

    feat: Implement findAllEvents function and integrate topic creation in KafkaTopicManager

--- api/src/shared/infrastructure/kafka/kafka-topic-manager.ts
+++ api/src/shared/infrastructure/kafka/kafka-topic-manager.ts
@@ -1,8 +1,12 @@
-import {Injectable, OnModuleInit, OnModuleDestroy} from '@nestjs/common';
+import {Injectable, OnModuleInit, OnModuleDestroy, Logger, OnApplicationBootstrap} from '@nestjs/common';
 import { Kafka, Admin } from 'kafkajs';
+import path = require('path');
+import {findAllEvents} from "../find-all-events";
 
 @Injectable()
-export class KafkaTopicManager implements OnModuleInit, OnModuleDestroy {
+export class KafkaTopicManager implements OnModuleInit, OnModuleDestroy, OnApplicationBootstrap {
+    private readonly logger = new Logger(KafkaTopicManager.name);
+    private admin!: Admin;
     private readonly kafka = new Kafka({
         clientId: 'admin-client',
         brokers: [process.env.KAFKA_BROKER || 'kafka:9092'],
@@ -15,29 +19,48 @@ export class KafkaTopicManager implements OnModuleInit, OnModuleDestroy {
             maxRetryTime: 30000,
         },
     });
-    private admin!: Admin;
 
     async onModuleInit() {
         this.admin = this.kafka.admin();
         await this.admin.connect();
-
-        await this.createTopicsIfNeeded();
     }
 
     async onModuleDestroy() {
         await this.admin.disconnect();
     }
 
-    private async createTopicsIfNeeded() {
-        await this.admin.createTopics({
-            topics: [
-                {
-                    topic: 'user_created',
-                    numPartitions: 1,
-                    replicationFactor: 1,
-                },
-            ],
+    async onApplicationBootstrap() {
+        await this.createTopicsIfNotExist();
+    }
+
+
+    async createTopicsIfNotExist() {
+        const existingTopics = await this.admin.listTopics();
+
+        const baseDir = path.resolve(__dirname, '..', '..');
+        const events = await findAllEvents(baseDir);
+
+        const topicsToCreate = events
+            .map(e => e.topic)
+            .filter(topic => !existingTopics.includes(topic))
+            .map(topic => ({
+                topic,
+                numPartitions: 1,
+                replicationFactor: 1,
+            }));
+
+        if (topicsToCreate.length === 0) {
+            this.logger.log('No hay topics nuevos para crear');
+            return;
+        }
+
+        this.logger.log(`Creando topics: ${topicsToCreate.map(t => t.topic).join(', ')}`);
+
+        const result = await this.admin.createTopics({
+            topics: topicsToCreate,
             waitForLeaders: true,
         });
+
+        this.logger.log(`Topics creados: ${result}`);
     }
 }

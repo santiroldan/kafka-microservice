926c5c789a5d6012833ea4436ad3f6b9bc6feec3
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-15 01:50:39 +0200

    feat: Refactor publish method to use DomainEvent and retrieve topic dynamically

--- api/src/shared/infrastructure/kafka/kafka-event-publisher.ts
+++ api/src/shared/infrastructure/kafka/kafka-event-publisher.ts
@@ -1,6 +1,8 @@
 import {EventPublisher} from '../../domain/event-publisher';
 import {Inject, Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
+import {getEventTopic} from "../../domain/event-topic-decorator";
+import {DomainEvent} from "../../domain/event";
 
 @Injectable()
 export class KafkaEventPublisher implements EventPublisher, OnModuleInit, OnModuleDestroy {
@@ -10,8 +12,14 @@ export class KafkaEventPublisher implements EventPublisher, OnModuleInit, OnModu
         await this.kafka.connect();
     }
 
-    publish(topic: string, payload: any): void {
-        this.kafka.emit(topic, payload);
+    publish(event: DomainEvent): void {
+        const topic = getEventTopic(event.constructor);
+
+        if (!topic) {
+            throw new Error('No se encontró topic en el evento');
+        }
+
+        this.kafka.emit(topic, event.getPayload());
     }
 
     async onModuleDestroy() {

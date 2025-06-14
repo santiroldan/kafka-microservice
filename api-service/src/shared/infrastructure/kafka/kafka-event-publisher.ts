316ec37ab74dd75e787f2bfd082608bf7998a60a
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-15 00:24:54 +0200

    feat: Implement KafkaEventPublisher lifecycle hooks for connection management

--- api-service/src/shared/infrastructure/kafka/kafka-event-publisher.ts
+++ api-service/src/shared/infrastructure/kafka/kafka-event-publisher.ts
@@ -1,12 +1,20 @@
 import {EventPublisher} from '../../domain/event-publisher';
-import {Inject, Injectable} from '@nestjs/common';
+import {Inject, Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class KafkaEventPublisher implements EventPublisher {
+export class KafkaEventPublisher implements EventPublisher, OnModuleInit, OnModuleDestroy {
     constructor(@Inject('KAFKA_SERVICE') private readonly kafka: ClientKafka) {}
+
+    async onModuleInit() {
+        await this.kafka.connect();
+    }
 
     publish(topic: string, payload: any): void {
         this.kafka.emit(topic, payload);
+    }
+
+    async onModuleDestroy() {
+        await this.kafka.close();
     }
 }

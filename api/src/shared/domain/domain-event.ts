865df0640a5a609d5b62b638dd5097a9918f1316
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-15 16:32:08 +0200

    feat: Refactor DomainEvent to abstract class and encapsulate occurredOn property

--- api/src/shared/domain/domain-event.ts
+++ api/src/shared/domain/domain-event.ts
@@ -1,11 +1,21 @@
-export interface DomainEvent {
-    occurredOn: Date;
-    getEventName(): string;
-    getOccurredOn(): Date;
-    getPayload(): any;
+export abstract class DomainEvent {
+    static eventName: string;
+
+    private readonly occurredOn: Date;
+
+    protected constructor() {
+        this.occurredOn = new Date();
+    }
+
+    getOccurredOn(): Date {
+        return this.occurredOn;
+    }
+
+    abstract getEventName(): string;
+    abstract getPayload(): unknown;
 }
 
-export interface DomainEventConstructor {
-    new (...args: any[]): DomainEvent;
+export interface DomainEventConstructor<T extends DomainEvent = DomainEvent> {
+    new (...args: any[]): T;
     eventName: string;
 }

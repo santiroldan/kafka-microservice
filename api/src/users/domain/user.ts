2aa7d8063f5753e2f0225ab30ef6c790bb408596
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-15 17:04:04 +0200

    feat: Refactor User class to use UserEmail value object and add factory method for user creation

--- api/src/users/domain/user.ts
+++ api/src/users/domain/user.ts
@@ -1,6 +1,24 @@
+import {UserEmail} from "./value-objects/user-email";
+
 export class User {
-	constructor(
-		public readonly email: string,
-		public readonly name: string,
+	private constructor(
+		private readonly email: UserEmail,
+		private readonly name: string,
 	) {}
+
+	static create(email: string, name: string): User {
+		if (!email || !name) {
+			throw new Error('Email and name are required to create a user.');
+		}
+
+		return new User(new UserEmail(email), name);
+	}
+
+	getEmail(): string {
+		return this.email.value;
+	}
+
+	getName(): string {
+		return this.name;
+	}
 }

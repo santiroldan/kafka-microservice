d75fad42a773d4585ac456e30061b53d550a1b9f
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-15 14:56:42 +0200

    feat: Add event name and timestamp to UserCreated event class

--- api/src/users/domain/events/user-created.ts
+++ api/src/users/domain/events/user-created.ts
@@ -1,10 +1,18 @@
-import {DomainEvent} from "../../../shared/domain/event";
+import {DomainEvent} from "../../../shared/domain/domain-event";
 
 export class UserCreated implements DomainEvent {
+    static eventName = 'user_created';
+
+    readonly occurredOn = new Date();
+
     constructor(
         public readonly id: string,
         public readonly name: string,
     ) {}
+
+    getEventName(): string {
+        return UserCreated.eventName;
+    }
 
     getPayload() {
         return {

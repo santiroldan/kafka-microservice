64abe78f2cb10615956dbc6e2829d6f264b4d2e2
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-15 16:52:16 +0200

    feat: Refactor save method in MongoUserRepository to accept User object and remove event publishing

--- api/src/users/infrastructure/mongo/mongo-user-repository.ts
+++ api/src/users/infrastructure/mongo/mongo-user-repository.ts
@@ -1,27 +1,18 @@
-import {Inject, Injectable} from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserRepository } from '../../domain/user-repository';
 import { User } from '../../domain/user';
 import { UserDocument } from './user-schema';
-import {EVENT_PUBLISHER, EventPublisher} from "../../../shared/domain/event-publisher";
-import {UserCreated} from "../../domain/events/user-created";
 
 @Injectable()
 export class MongoUserRepository implements UserRepository {
     constructor(
         @InjectModel('User') private readonly userModel: Model<UserDocument>,
-        @Inject(EVENT_PUBLISHER) private readonly publisher: EventPublisher,
     ) {}
 
-    async save(name: string): Promise<void> {
-        const userSaved = await this.userModel.insertOne(
-            {
-                name,
-            },
-        );
-
-        return this.publisher.publish(new UserCreated(userSaved.id, userSaved.name));
+    async save(user: User): Promise<void> {
+         await this.userModel.insertOne(user);
     }
 
     async findByName(name: string): Promise<User> {

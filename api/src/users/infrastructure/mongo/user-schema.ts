d3c80f6645ebed1cadc918aecf8ce0beabde89c9
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-15 16:52:06 +0200

    feat: Add email field to UserModel and update UserSchema for validation

--- api/src/users/infrastructure/mongo/user-schema.ts
+++ api/src/users/infrastructure/mongo/user-schema.ts
@@ -3,9 +3,11 @@ import { type Document, Schema } from "mongoose";
 export type UserDocument = UserModel & Document;
 
 export type UserModel = {
+	email: string;
 	name: string;
 };
 
 export const UserSchema = new Schema<UserModel>({
+	email: { type: String, required: true, unique: true },
 	name: { type: String, required: true },
 });

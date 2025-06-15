30f68856fcc4eafd468321b06901cf1f12475ba0
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-15 16:51:44 +0200

    feat: Enhance UserCreator to publish UserCreated event after user creation

--- api/src/users/application/user-creator.ts
+++ api/src/users/application/user-creator.ts
@@ -1,12 +1,22 @@
 import {USER_REPOSITORY, UserRepository} from "../domain/user-repository";
 import {Inject} from "@nestjs/common";
+import {EVENT_PUBLISHER, EventPublisher} from "../../shared/domain/event-publisher";
+import {UserCreated} from "../domain/events/user-created";
+import {User} from "../domain/user";
+import {UserCreatorInput} from "./user-creator-input";
 
 export class UserCreator {
     constructor(
         @Inject(USER_REPOSITORY) private repository: UserRepository,
+        @Inject(EVENT_PUBLISHER) private readonly publisher: EventPublisher,
+
     ) {}
 
-    async execute(name: string): Promise<void> {
-        return await this.repository.save(name);
+    async execute(input: UserCreatorInput): Promise<void> {
+        const userToCreate = new User(input.email, input.name);
+
+        await this.repository.save(userToCreate);
+
+        return this.publisher.publish(new UserCreated(userToCreate.email, userToCreate.name));
     }
 }
